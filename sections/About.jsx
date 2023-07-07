7098db023d60e0e672f429e230ac36cb18dc9688
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-07-07 20:59:50 +0530

    Metaverse Horizon

---
+++ sections/About.jsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { TypingText } from "../components";
+
+import styles from "../styles";
+import { fadeIn, staggerContainer } from "../utils/motion";
+
+const About = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0" />
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+      <TypingText title="| About Metaverse Horizon" textStyles="text-center" />
+
+      <motion.p
+        variants={fadeIn("up", "tween", 0.2, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
+      >
+        <span className="font-extrabold text-white">
+          Step into the Metaverse Horizon,
+        </span>{" "}
+        where reality expands and imagination knows no limits. Experience a
+        digital universe of infinite possibilities, connecting with others,
+        embarking on thrilling adventures, and shaping your own destiny.
+        Discover breathtaking landscapes, engage in exhilarating quests, and
+        forge meaningful connections in this vibrant social hub. Immerse
+        yourself in stunning visuals, lifelike avatars, and immersive
+        experiences. Welcome to the Metaverse Horizon, where the future unfolds
+        before your eyes. Let's{" "}
+        <span className="font-extrabold text-white">explore</span> the Metaverse
+        Horizon by scrolling down
+      </motion.p>
+
+      <motion.img
+        variants={fadeIn("up", "tween", 0.3, 1)}
+        src="/arrow-down.svg"
+        alt="arrow down"
+        className="w-[18px] h-[28px] object-contain mt-[28px]"
+      />
+    </motion.div>
+  </section>
+);
+
+export default About;
