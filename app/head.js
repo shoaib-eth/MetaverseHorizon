d7b244fb600f740796c838fea211f8130124a10f
Shoaib Khan <124503086+ShoaibDev69@users.noreply.github.com>
2023-07-13 18:50:45 +0530

    Update head.js

--- app/head.js
+++ app/head.js
@@ -1,9 +1,9 @@
 const Head = () => (
-  <div>
+  <>
     <title>Metaverse Horizon</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link href="/favicon.ico" rel="icon" />
-  </div>
+    <link rel="icon" href="/favicon.ico" />
+  </>
 );
 
 export default Head;
