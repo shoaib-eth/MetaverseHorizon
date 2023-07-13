518b235a53223f27f8db8cd88479e877da9b9e44
Shoaib Khan <124503086+ShoaibDev69@users.noreply.github.com>
2023-07-13 18:56:48 +0530

    Update CustomTexts.jsx

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,11 +1,11 @@
-'use client';
-
 import { motion } from 'framer-motion';
 import { textContainer, textVariant2 } from '../utils/motion';
 
-export const TypingText = ({ title, textStyles }) => (
+const TypingText = ({ title, textStyles }) => (
   <motion.p
     variants={textContainer}
+    initial="hidden"
+    animate="show" // Changed `exited` to `animate`
     className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
   >
     {Array.from(title).map((letter, index) => (
@@ -16,13 +16,15 @@ export const TypingText = ({ title, textStyles }) => (
   </motion.p>
 );
 
-export const TitleText = ({ title, textStyles }) => (
+const TitleText = ({ title, textStyles }) => (
   <motion.h2
     variants={textVariant2}
     initial="hidden"
-    whileInView="show"
+    animate="show" // Changed `entered` to `animate`
     className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}
   >
     {title}
   </motion.h2>
 );
+
+export { TypingText, TitleText };
