a6a1e7971052b712543414aea2cbe714da2f77b1
Shoaib Khan <124503086+ShoaibDev69@users.noreply.github.com>
2023-07-18 19:15:45 +0530

    Update Insights.jsx

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+'use client';
+
 import { motion } from 'framer-motion';
 
 import styles from '../styles';
@@ -11,13 +12,13 @@ const Insights = () => (
     <motion.div
       variants={staggerContainer}
       initial="hidden"
-      animate="show"
-      exit="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto flex flex-col`}
     >
       <TypingText title="| Insight" textStyles="text-center" />
       <TitleText title={<>Insight about metaverse</>} textStyles="text-center" />
-      <div className="mt-8 flex flex-col gap-8">
+      <div className="mt-[50px] flex flex-col gap-[30px]">
         {insights.map((item, index) => (
           <InsightCard key={`insight-${index}`} {...item} index={index + 1} />
         ))}
@@ -27,4 +28,3 @@ const Insights = () => (
 );
 
 export default Insights;
-
