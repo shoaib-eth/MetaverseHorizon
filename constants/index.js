d9a131b3c1c91472c375ef3291c9b0a7f3524854
Shoaib Khan <124503086+ShoaibDev69@users.noreply.github.com>
2023-07-18 19:09:58 +0530

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,88 +1,88 @@
 export const exploreWorlds = [
   {
-    id: "world-1",
-    imgUrl: "/planet-01.png",
-    title: "The Hogwarts",
+    id: 'world-1',
+    imgUrl: '/planet-01.png',
+    title: 'The Hogwarts',
   },
   {
-    id: "world-2",
-    imgUrl: "/planet-02.png",
-    title: "The Upside Down",
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: 'The Upside Down',
   },
   {
-    id: "world-3",
-    imgUrl: "/planet-03.png",
-    title: "Kadirojo Permai",
+    id: 'world-3',
+    imgUrl: '/planet-03.png',
+    title: 'Kadirojo Permai',
   },
   {
-    id: "world-4",
-    imgUrl: "/planet-04.png",
-    title: "Paradise Island",
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'Paradise Island',
   },
   {
-    id: "world-5",
-    imgUrl: "/planet-05.png",
-    title: "Hawkins Labs",
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'Hawkins Labs',
   },
 ];
 
 export const startingFeatures = [
-  "Find a world that suits you and you want to enter",
-  "Unlock limitless possibilities in the Metaverse Horizon",
-  "Unleash the magic, experience the Metaverse Horizon through easy interaction",
+  'Find a world that suits you and you want to enter',
+  'Enter the world by reading basmalah to be safe',
+  'No need to beat around the bush, just stay on the gas and have fun',
 ];
 
 export const newFeatures = [
   {
-    imgUrl: "/vrpano.svg",
-    title: "A new world",
+    imgUrl: '/vrpano.svg',
+    title: 'A new world',
     subtitle:
-      "we have the latest update with new world for you to try never mind",
+        'we have the latest update with new world for you to try never mind',
   },
   {
-    imgUrl: "/headset.svg",
-    title: "More realistic",
+    imgUrl: '/headset.svg',
+    title: 'More realistic',
     subtitle:
-      "In the latest update, your eyes are narrow, making the world more realistic than ever",
+        'In the latest update, your eyes are narrow, making the world more realistic than ever',
   },
 ];
 
 export const insights = [
   {
-    imgUrl: "/planet-06.png",
-    title: "A Paradigm Shift in Digital Realms",
+    imgUrl: '/planet-06.png',
+    title: 'The launch of the Metaverse makes Elon musk ketar-ketir',
     subtitle:
-      "Embark on an extraordinary journey into a connected universe, where virtual and physical merge, expanding horizons of imagination and connectivity.",
+        'Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.',
   },
   {
-    imgUrl: "/planet-07.png",
-    title: "Where Reality Meets Infinite Possibilities",
+    imgUrl: '/planet-07.png',
+    title: '7 tips to easily master the madness of the Metaverse',
     subtitle:
-      "Step into a boundless virtual realm, where you can shape your destiny, connect with like-minded explorers, and redefine the limits of whats possible.",
+        'Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum',
   },
   {
-    imgUrl: "/planet-08.png",
-    title: "Unlocking a New Era of Experiences",
+    imgUrl: '/planet-08.png',
+    title: 'With one platform you can explore the whole world virtually',
     subtitle:
-      "Uncover the secrets of the Metaverse, a gateway to immersive adventures, transformative interactions, and limitless innovation, forever changing how we live and connect.",
+        'Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem',
   },
 ];
 
 export const socials = [
   {
-    name: "twitter",
-    url: "/twitter.svg",
+    name: 'twitter',
+    url: '/twitter.svg',
   },
   {
-    name: "linkedin",
-    url: "/linkedin.svg",
+    name: 'linkedin',
+    url: '/linkedin.svg',
   },
   {
-    name: "instagram",
-    url: "/instagram.svg",
+    name: 'instagram',
+    url: '/instagram.svg',
   },
   {
-    name: "facebook",
-    url: "/facebook.svg",
+    name: 'facebook',
+    url: '/facebook.svg',
   },
 ];
