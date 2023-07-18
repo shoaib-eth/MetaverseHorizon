1b62f9dc765176e416522495b4673a8442f776ca
Shoaib Khan <124503086+ShoaibDev69@users.noreply.github.com>
2023-07-18 19:02:13 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+'use client';
+
 import { motion } from 'framer-motion';
 import { socials } from '../constants';
 
@@ -9,40 +10,37 @@ const Footer = () => (
   <motion.footer
     variants={footerVariants}
     initial="hidden"
-    animate="show"
+    whileInView="show"
     className={`${styles.xPaddings} py-8 relative`}
   >
     <div className="footer-gradient" />
     <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
       <div className="flex items-center justify-between flex-wrap gap-5">
-        <h4 className="font-bold md:text-4xl text-2xl text-white">
-          Enter in the Metaverse
+        <h4 className="font-bold md:text-[64px] text-[44px] text-white">
+          Enter the Metaverse
         </h4>
-        <button
-          type="button"
-          className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]"
-        >
+        <button type="button" className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]">
           <img
             src="/headset.svg"
             alt="headset"
-            className="w-6 h-6 object-contain"
+            className="w-[24px] h-[24px] object-contain"
           />
-          <span className="font-normal text-base text-white">
-            Enter in the Metaverse
+          <span className="font-normal text-[16px] text-white">
+            Enter Metaverse
           </span>
         </button>
       </div>
 
       <div className="flex flex-col">
-        <div className="mb-10 h-0.5 bg-white opacity-10" />
+        <div className="mb-[50px] h-[2px] bg-white opacity-10" />
 
         <div className="flex items-center justify-between flex-wrap gap-4">
-          <h4 className="font-extrabold text-xl text-white">
-            METAVERSE HORIZON
+          <h4 className="font-extrabold text-[24px] text-white">
+            METAVERUS
           </h4>
-          {/* <p className="font-normal text-sm text-white opacity-50">
+          <p className="font-normal text-[14px] text-white opacity-50">
             Copyright © 2021 - 2022 Metaversus. All rights reserved.
-          </p> */}
+          </p>
 
           <div className="flex gap-4">
             {socials.map((social) => (
@@ -50,7 +48,7 @@ const Footer = () => (
                 key={social.name}
                 src={social.url}
                 alt={social.name}
-                className="w-6 h-6 object-contain cursor-pointer"
+                className="w-[24px] h-[24px] object-contain cursor-pointer"
               />
             ))}
           </div>
