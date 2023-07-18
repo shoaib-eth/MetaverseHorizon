bbea66cf8ca13530bd623f54d71688ecd159bbc8
Shoaib Khan <124503086+ShoaibDev69@users.noreply.github.com>
2023-07-18 18:59:06 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,32 +1,13 @@
-<<<<<<< HEAD
-// import '../styles/globals.css';
-
-// const RootLayout = ({ children }) => (
-//   <html lang="en">
-//     <head>
-//       <link rel="preconnect" href="https://stijndv.com" />
-//       <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
-//     </head>
-//     <body>{children}</body>
-//   </html>
-// );
-
-// export default RootLayout;
-=======
-import React from 'react';
->>>>>>> 223ee6458d53a381c554a8a5c58ee8ae56eef45f
 import '../styles/globals.css';
 
-const RootLayout = ({ children }) => {
-  return (
-    <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://stijndv.com" />
-        <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
-      </head>
-      <body>{children}</body>
-    </html>
-  );
-};
+const RootLayout = ({ children }) => (
+  <html lang="en">
+    <head>
+      <link rel="preconnect" href="https://stijndv.com" />
+      <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
+    </head>
+    <body>{children}</body>
+  </html>
+);
 
 export default RootLayout;
