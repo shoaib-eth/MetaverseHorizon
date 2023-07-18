3407eb16a099c72548faa1b1937504c364f11196
Shoaib Khan <124503086+ShoaibDev69@users.noreply.github.com>
2023-07-18 19:00:38 +0530

    Update CustomTexts.jsx

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,74 +1,11 @@
-<<<<<<< HEAD
-// 'use client';
+'use client';
 
-// import { motion } from 'framer-motion';
-// import { textContainer, textVariant2 } from '../utils/motion';
-
-// export const TypingText = ({ title, textStyles }) => (
-//   <motion.p
-//     variants={textContainer}
-//     className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
-//   >
-//     {Array.from(title).map((letter, index) => (
-//       <motion.span variants={textVariant2} key={index}>
-//         {letter === ' ' ? '\u00A0' : letter}
-//       </motion.span>
-//     ))}
-//   </motion.p>
-// );
-
-// export const TitleText = ({ title, textStyles }) => (
-//   <motion.h2
-//     variants={textVariant2}
-//     initial="hidden"
-//     whileInView="show"
-//     className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}
-//   >
-//     {title}
-//   </motion.h2>
-// );
 import { motion } from 'framer-motion';
 import { textContainer, textVariant2 } from '../utils/motion';
 
-export const TypingText = ({ title, textStyles }) => {
-  return (
-    <motion.p
-      variants={textContainer}
-      className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
-    >
-      {title.split('').map((letter, index) => (
-        <motion.span
-          key={index}
-          variants={textVariant2}
-        >
-          {letter === ' ' ? '\u00A0' : letter}
-        </motion.span>
-      ))}
-    </motion.p>
-  );
-};
-
-export const TitleText = ({ title, textStyles }) => {
-  return (
-    <motion.h2
-      variants={textVariant2}
-      initial="hidden"
-      whileInView="show"
-      className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}
-    >
-      {title}
-    </motion.h2>
-  );
-};
-=======
-import { motion } from 'framer-motion';
-import { textContainer, textVariant2 } from '../utils/motion';
-
-const TypingText = ({ title, textStyles }) => (
+export const TypingText = ({ title, textStyles }) => (
   <motion.p
     variants={textContainer}
-    initial="hidden"
-    animate="show" // Changed `exited` to `animate`
     className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
   >
     {Array.from(title).map((letter, index) => (
@@ -79,16 +16,13 @@ const TypingText = ({ title, textStyles }) => (
   </motion.p>
 );
 
-const TitleText = ({ title, textStyles }) => (
+export const TitleText = ({ title, textStyles }) => (
   <motion.h2
     variants={textVariant2}
     initial="hidden"
-    animate="show" // Changed `entered` to `animate`
+    whileInView="show"
     className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}
   >
     {title}
   </motion.h2>
 );
-
-export { TypingText, TitleText };
->>>>>>> 223ee6458d53a381c554a8a5c58ee8ae56eef45f
