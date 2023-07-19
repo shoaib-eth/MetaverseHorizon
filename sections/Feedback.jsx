16a56291cbe832ad6c1d8ccb9a05b1b181784210
Shoaib Khan <124503086+ShoaibDev69@users.noreply.github.com>
2023-07-19 16:53:08 +0530

    Update Feedback.jsx

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -21,17 +21,17 @@ const Feedback = () => (
         <div className="feedback-gradient" />
         <div>
           <h4 className="font-bold sm:text-[32px] text-[26px] sm:leading-[40.32px] leading-[36.32px] text-white">
-            Samantha
+            Shoaib Khan
           </h4>
           <p className="mt-[8px] font-normal sm:text-[18px] text-[12px] sm:leading-[22.68px] leading-[16.68px] text-white">
-            Founder Metaverus
+            Developer of Metaverse Horizon
           </p>
         </div>
 
         <p className="mt-[24px] font-normal sm:text-[24px] text-[18px] sm:leading-[45.6px] leading-[39.6px] text-white">
-          “With the development of today's technology, metaverse is very
-          useful for today's work, or can be called web 3.0. by using
-          metaverse you can use it as anything”
+           “Witness the transformative power of the metaverse, the Web 3.0
+          revolution. Seamlessly adapt to its endless possibilities, unlocking
+          new dimensions to redefine work and shape the future.”
         </p>
       </motion.div>
 
