1f8e97545784578d117b17170cdee90f802f9e49
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-07-23 18:59:38 +0530

    Initial Commit

---
+++ constants/index.js
@@ -0,0 +1,88 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-01.png',
+    title: 'The Hogwarts',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: 'The Upside Down',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-03.png',
+    title: 'Kadirojo Permai',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'Paradise Island',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'Hawkins Labs',
+  },
+];
+
+export const startingFeatures = [
+  "Find a world that suits you and you want to enter",
+  "Unlock limitless possibilities in the Metaverse Horizon",
+  "Unleash the magic, experience the Metaverse Horizon through easy interaction",
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'A new world',
+    subtitle:
+        'we have the latest update with new world for you to try never mind',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'More realistic',
+    subtitle:
+        'In the latest update, your eyes are narrow, making the world more realistic than ever',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: "/planet-06.png",
+    title: "A Paradigm Shift in Digital Realms",
+    subtitle:
+      "Embark on an extraordinary journey into a connected universe, where virtual and physical merge, expanding horizons of imagination and connectivity.",
+  },
+  {
+    imgUrl: "/planet-07.png",
+    title: "Where Reality Meets Infinite Possibilities",
+    subtitle:
+      "Step into a boundless virtual realm, where you can shape your destiny, connect with like-minded explorers, and redefine the limits of whats possible.",
+  },
+  {
+    imgUrl: "/planet-08.png",
+    title: "Unlocking a New Era of Experiences",
+    subtitle:
+      "Uncover the secrets of the Metaverse, a gateway to immersive adventures, transformative interactions, and limitless innovation, forever changing how we live and connect.",
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];
