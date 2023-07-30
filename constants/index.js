98bf389b820a4dad09b984ddba06de9144a41eef
Shoaib Khan <124503086+ShoaibDev69@users.noreply.github.com>
2023-07-30 17:17:21 +0530

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -37,13 +37,13 @@ export const newFeatures = [
     imgUrl: '/vrpano.svg',
     title: 'A new world',
     subtitle:
-        'we have the latest update with new world for you to try never mind',
+        'A new world for you to try never mind',
   },
   {
     imgUrl: '/headset.svg',
     title: 'More realistic',
     subtitle:
-        'In the latest update, your eyes are narrow, making the world more realistic than ever',
+        'Making the world more realistic than ever',
   },
 ];
 
