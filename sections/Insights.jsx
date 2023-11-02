5f3062ecb19ef757e65090b38bd606ad74d608cc
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-11-02 16:28:53 +0530

    Initial commit

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -1,11 +1,11 @@
-'use client';
+"use client";
 
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
-import styles from '../styles';
-import { insights } from '../constants';
-import { staggerContainer } from '../utils/motion';
-import { InsightCard, TitleText, TypingText } from '../components';
+import styles from "../styles";
+import { insights } from "../constants";
+import { staggerContainer } from "../utils/motion";
+import { InsightCard, TitleText, TypingText } from "../components";
 
 const Insights = () => (
   <section className={`${styles.paddings} relative z-10`}>
@@ -17,7 +17,10 @@ const Insights = () => (
       className={`${styles.innerWidth} mx-auto flex flex-col`}
     >
       <TypingText title="| Insight" textStyles="text-center" />
-      <TitleText title={<>Insight About Metaverse Horizon</>} textStyles="text-center" />
+      <TitleText
+        title={<>Insight About Metaverse Horizon</>}
+        textStyles="text-center"
+      />
       <div className="mt-[50px] flex flex-col gap-[30px]">
         {insights.map((item, index) => (
           <InsightCard key={`insight-${index}`} {...item} index={index + 1} />
