f3d913b4703ab0d74edd32fdfc25cb5c3aa3ce78
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-11-02 16:26:42 +0530

    Initial commit

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,10 +1,10 @@
-'use client';
+"use client";
 
-import { motion } from 'framer-motion';
-import { TypingText } from '../components';
+import { motion } from "framer-motion";
+import { TypingText } from "../components";
 
-import styles from '../styles';
-import { fadeIn, staggerContainer } from '../utils/motion';
+import styles from "../styles";
+import { fadeIn, staggerContainer } from "../utils/motion";
 
 const About = () => (
   <section className={`${styles.paddings} relative z-10`}>
@@ -19,7 +19,7 @@ const About = () => (
       <TypingText title="| About Metaverse Horizon" textStyles="text-center" />
 
       <motion.p
-        variants={fadeIn('up', 'tween', 0.2, 1)}
+        variants={fadeIn("up", "tween", 0.2, 1)}
         className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
       >
         <span className="font-extrabold text-white">
@@ -28,14 +28,14 @@ const About = () => (
         where reality expands and imagination knows no limits. Experience a
         digital universe of infinite possibilities, connecting with others,
         embarking on thrilling adventures, and shaping your own destiny. Immerse
-        yourself in stunning visuals and immersive experiences. Welcome to the Metaverse Horizon, where the future unfolds
-        before your eyes. Let's{" "}
+        yourself in stunning visuals and immersive experiences. Welcome to the
+        Metaverse Horizon, where the future unfolds before your eyes. Let's{" "}
         <span className="font-extrabold text-white">explore</span> the Metaverse
         Horizon by scrolling down
       </motion.p>
 
       <motion.img
-        variants={fadeIn('up', 'tween', 0.3, 1)}
+        variants={fadeIn("up", "tween", 0.3, 1)}
         src="/arrow-down.svg"
         alt="arrow down"
         className="w-[18px] h-[28px] object-contain mt-[28px]"
