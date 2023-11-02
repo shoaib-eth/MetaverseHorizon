4a2ce7dcb13acd5b17994b57d8901b28ea6a57eb
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-11-02 16:28:30 +0530

    Initial commit

--- sections/index.js
+++ sections/index.js
@@ -1,11 +1,11 @@
-import Hero from './Hero';
-import About from './About';
-import Explore from './Explore';
-import GetStarted from './GetStarted';
-import WhatsNew from './WhatsNew';
-import World from './World';
-import Insights from './Insights';
-import Feedback from './Feedback';
+import Hero from "./Hero";
+import About from "./About";
+import Explore from "./Explore";
+import GetStarted from "./GetStarted";
+import WhatsNew from "./WhatsNew";
+import World from "./World";
+import Insights from "./Insights";
+import Feedback from "./Feedback";
 
 export {
   Hero,
